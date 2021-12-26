77e2d9046509a877251085979ce07fe4847a2ab6
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-12-26 16:44:26 +0530

    Update and rename Music.py to music.py

--- Music.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system ("git clone -b new https://github.com/LEGEND-OS/MUSIC-VC.git && cd MUSIC-VC && python3 main.py")
---
+++ music.py
@@ -0,0 +1,3 @@
+import os
+os.system("git clone https://github.com/LEGEND-OS/MUSIC-VC")
+os.system("cd MUSIC-VC && python3 main.py")
